c029d2f36426aa1ad3f3f0a4eb8b943ac5580985
Alf Eaton <alf@hubmed.org>
2014-01-07 14:42:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# jQuery Things
+
+A new approach to the HTML5 Microdata API
+
+## Methods
+
+* $(node).things(itemtype) => an array of Thing objects
+    $('#albumlist').things('http://schema.org/MusicAlbum') => [Thing, Thing, Thing]
+
+* Thing.get(property) => a literal value or a Thing
+    thing.get('name') => string
+    thing.get('byArtist') => Thing
+    thing.get('byArtist').get('name') => string
+
+* Thing.get(property+) => an array of literal values or Things
+    thing.get('name+') => [string]
+    thing.get('byArtist').get('album+') => [Thing, Thing, Thing]
+
+* Thing.nodes(property) => an array of nodes with this itemprop attribute
+
+* $(node).value(value) => set the itemValue of a node
---
+++ example.js
@@ -0,0 +1,55 @@
+/* convert to JSON */
+
+$(function() {
+	var albums = $('#albumlist').things('http://schema.org/MusicAlbum').map(function() {
+		//return this.get('byArtist').get('name');
+		return this.data();
+	});
+
+	var code = $('<code/>', { text: JSON.stringify(albums.toArray(), null, 2) });
+	$('<pre/>').append(code).appendTo('body');
+});
+
+/* display in a table */
+
+$(function() {
+	var table = $('<table/>').appendTo('body');
+	var thead = $('<thead/>').appendTo(table);
+	var tbody = $('<tbody/>').appendTo(table);
+
+	var row = $('<tr/>').appendTo(thead);
+	$('<th/>', { text: 'album' }).appendTo(row);
+	$('<th/>', { text: 'artist' }).appendTo(row);
+
+	var albums = $('#albumlist').things('http://schema.org/MusicAlbum').map(function(index, album) {
+		// album
+		var row = $('<tr/>').appendTo(tbody);
+
+		var cell = $('<td/>').appendTo(row);
+		$('<a/>', { href: album.get('url'), text: album.get('name') }).appendTo(cell);
+
+		// group (artist)
+		var artist = album.get('byArtist');
+		var cell = $('<td/>').appendTo(row);
+
+		$('<a/>', { href: artist.get('url'), text: artist.get('name') }).appendTo(cell);
+
+		// group's members
+		var members = artist.get('musicGroupMember+').map(function(item) {
+			return item.get('name');
+		});
+
+		if (members.length) {
+			$('<div/>', { text: 'Members: ' + members.join(', ') }).appendTo(cell);
+		}
+
+		// group's albums
+		var albums = artist.get('album+').map(function(item) {
+			return item.get('name');
+		});
+
+		if (albums.length) {
+			$('<div/>', { text: 'Albums: ' + albums.join(', ') }).appendTo(cell);
+		}
+	});
+});
